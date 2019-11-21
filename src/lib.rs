3ee83a3a62ac2fbca2e25c3ac4d801d6462cc76a
Laurence Tratt <laurie@tratt.net>
2019-11-21 09:30:34 +0000

    Move to our new licensing regime.
    Most of the copyright in this project is held by King's. Thom Wiggers retains
    his own copyright and has explicitly given his assent to this relicensing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-// Copyright (c) 2018 King's College London created by the Software Development Team
-// <http://soft-dev.org/>
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0>, or the MIT license <LICENSE-MIT or
-// http://opensource.org/licenses/MIT>, or the UPL-1.0 license <http://opensource.org/licenses/UPL>
-// at your option. This file may not be copied, modified, or distributed except according to those
-// terms.
-
 //! A vector of bits ("Vob") is a sequence of bits which exposes a `Vec`-like interface. Whereas
 //! `Vec<bool>` requires 1 byte of storage per bit, `Vob` requires only 1 bit of storage per bit.
 //!
